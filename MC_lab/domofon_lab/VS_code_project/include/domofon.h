8e5bdb792108c7bcdd9d5622473993978296c1e1
Frog6616261 <goodfor332@gmail.com>
2026-07-14 16:51:58 +0300

    reform all rep

---
+++ MC_lab/domofon_lab/VS_code_project/include/domofon.h
@@ -0,0 +1,16 @@
+#ifndef FONT_5X7_H
+#define FONT_5X7_H
+
+unsigned char first_tab[] = {'1', '1', '1', '1'}; 
+unsigned char second_tab[] = {'2', 'A', 'B', 'C'}; 
+unsigned char third_tab[] = {'3', 'D', 'E', 'F'}; 
+
+unsigned char fourth_tab[] = {'4', 'G', 'H', 'I'}; 
+unsigned char fifth_tab[] = {'5', 'J', 'K', 'L'}; 
+unsigned char sixth_tab[] = {'6', 'M', 'N', 'O'}; 
+
+unsigned char seventh_tab[] = {'7', 'P', 'R', 'S'}; 
+unsigned char eigth_tab[] = {'8', 'T', 'U', 'V'}; 
+unsigned char ninth_tab[] = {'9', 'W', 'X', 'Y'}; 
+
+unsigned char zeroth_tab[] = {'0', 'P', 'E', 'R'}; 
